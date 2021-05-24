e88068012550eba77949c401b110f0b7adf4a091
hamidrehman239 <72249396+hamidrehman239@users.noreply.github.com>
2021-05-24 07:04:34 +0500

    admin pannal

---
+++ adminpanal/lib/models/removeteacher.dart
@@ -0,0 +1,15 @@
+class removeteacher {
+  final String icon, title, date, size;
+
+  removeteacher({this.icon, this.title, this.date, this.size});
+}
+
+List demoRecentFiles = [
+  removeteacher(
+    icon: "assets/icons/media_file.svg",
+    title: "Add Teachers",
+    date: "23-05-2021",
+    size: "249kb",
+  ),
+
+];
