5c4910d357803e2280d51e9f1fe133de7370292e
QuratUlAinSjaid <80091216+QuratulainSajid712@users.noreply.github.com>
2021-05-30 22:20:05 +0500

    teacher panel

--- home_work_app/lib/UI/SingIn.dart
+++ home_work_app/lib/UI/SingIn.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:home_work_app/Fade_Animation/FadeAnimation.dart';
+import 'package:home_work_app/UI/screens/teacher/main_teacher_page.dart';
 import 'SignUp.dart';
 import 'my_flutter_app_icons.dart';
 import 'selected_type.dart';
@@ -159,7 +160,7 @@ class signin extends StatelessWidget {
                           color:  Color.fromRGBO(143, 148, 251, 1),
                           onPressed: () {
                             Navigator.push(context, MaterialPageRoute(
-                                builder: (context) => selecttype()));
+                                builder: (context) => MainTeacherPage()));
                             //HardType
                           },
                           child: new Text(
